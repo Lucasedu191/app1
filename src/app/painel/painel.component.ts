a6e6a482837fea7f515f69c8c78d910186147117
Lucas Silva <lucasatletico@hotmail.com>
2020-01-03 09:50:56 -0300

    finalizando a barra de progresso

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -10,13 +10,14 @@ import{ FRASES } from './frases-mock'
 export class PainelComponent implements OnInit {
   public frases: Frase[] = FRASES
   public instrucao: string ='Traduza a frase'
-  public resposta: string
+  public resposta: string = ''
   public rodada: number = 0
   public rodadaFrase: Frase
   public progresso: number = 0
-  constructor() { 
-    this.rodadaFrase = this.frases[this.rodada]  
-    console.log(this.rodadaFrase)
+  constructor() {
+    this.atualizaRodada() 
+      
+
   }
 
   ngOnInit() {
@@ -36,11 +37,17 @@ export class PainelComponent implements OnInit {
     this.progresso = this.progresso + (100 / this.frases.length)
     console.log(this.progresso)
    //atualiza o objeto rodada frase
-    this.rodadaFrase = this.frases[this.rodada]
+   this.atualizaRodada()
     
-
     } else{
       alert('A tradução está errada')
     }
+  }
+
+  public atualizaRodada(): void {
+    //define a frase da rodada com base na logica
+    this.rodadaFrase = this.frases[this.rodada]
+      //limpar a resposta
+    this.resposta = ''
   }
 }
